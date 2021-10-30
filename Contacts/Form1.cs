d531b0cb9cf5d20e3c662c5a9792515d81e2f4a3
LeviUmr <Lumr@outlook.com.br>
2021-10-30 11:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ Contacts/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contacts
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Contact List 1.0.\nWritten By: Levi Urbanovich\n", "about");
+		}
+
+		private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			this.Validate();
+			this.peopleBindingSource.EndEdit();
+			this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// TODO: esta linha de código carrega dados na tabela 'contactDBDataSet.People'. Você pode movê-la ou removê-la conforme necessário.
+			this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+		}
+
+		private void clientCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void contactIDTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void contactIDLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void nameLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void nameTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void companyLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void companyTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void telephoneLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void telephoneTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void emailLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void emailTextBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void clientLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lastCallLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lastCallDateTimePicker_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void contactIDLabel2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void contactIDLabel1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
